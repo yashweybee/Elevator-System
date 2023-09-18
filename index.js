cf98fc0e1ed511facfdb94d792193eb5a221f747
yashweybee <yashparmar@weybee.in>
2023-09-18 17:49:55 +0530

    completed with dynamic floors and lifts

--- index.js
+++ index.js
@@ -1,89 +1,104 @@
-// const up_1 = document.getElementById("up-1");
-// const up_2 = document.getElementById("up-2");
-// const up_3 = document.getElementById("up-3");
-// const up_4 = document.getElementById("up-4");
-
-// const down_5 = document.getElementById("down-5");
-// const down_4 = document.getElementById("down-4");
-// const down_3 = document.getElementById("down-3");
-// const down_2 = document.getElementById("down-2");
-
-const liftBtn = document.querySelectorAll(".aeroBtn");
-
 const mainContainer = document.getElementById("main-container");
+const maintenance = document.getElementById("maintenance");
 
-const floorClasses = ["floor-1", "floor-2", "floor-3", "floor-4", "floor-5"]
-const numberOfLift = 3;
-// let currentFloor = 1;
+// const numberOfLift = 6;
+// const numberOfFloor = 10;
+const numberOfLift = prompt("Enter Numer of Lift: ");
+const numberOfFloor = prompt("Enter numer of floors: ");
 let userEnterdFloor;
 let nearestLift;
 let randomNum = 0;
 let liftFloorData = []; //saves current floor data
-
 for (let i = 0; i < numberOfLift; i++) {
     liftFloorData.push(1);
 }
-console.log(liftFloorData);
 
 for (let i = 0; i < numberOfLift; i++) {
     const liftHtml =
         `
-    <div id="lift-container-${i}" class="lift-container">
+<div id="lift-container-${i}" class="lift-container">
    <div class="doors">
-      <div id="lift-${i}" class="lift"><span>${liftFloorData[i]}</span></div>
+      <div id="lift-${i}" class="${i} lift">
+      <span>${1}</span>
+      </div>
       <div class="form-check form-switch position-absolute bottom-0 start-50 translate-middle-x">
-         <input class="form-check-input" type="checkbox" role="switch"
+         <input class="${i} form-check-input" type="checkbox" role="switch"
             id="switch-${i}">
       </div>
    </div>
-</div>`
+</div>`;
     mainContainer.insertAdjacentHTML("afterbegin", liftHtml)
 }
+
+for (let i = 0; i < numberOfFloor; i++) {
+    const florHtml =
+        `
+    <div class="floor">
+        <p>${i + 1}</p>
+        <div id="up-${i + 1}" class=" ${i + 1} aeroBtn btn-up">&uarr;</div>
+        <div id="down-${i + 1}" class=" ${i + 1} aeroBtn btn-down">&darr;</div>
+    </div>
+    `
+    maintenance.insertAdjacentHTML("afterbegin", florHtml)
+}
+
 const lift = document.querySelectorAll(".lift-container .doors .lift");
 const switchBtn = document.querySelectorAll(".form-check-input");
-// console.log(lift);
-
+const liftBtn = document.querySelectorAll(".aeroBtn");
 
 // adding event listners
 switchBtn.forEach(btn => {
-    btn.addEventListener('click', switchBtnhandle)
+    btn.addEventListener('click', switchBtnhandle);
 })
 
 liftBtn.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        userEnterdFloor = Number(e.target.id.slice(-1))
-        handleLiftBtn(userEnterdFloor);
+        // console.log(e.target.classList[0]);
+        userEnterdFloor = Number(e.target.classList[0]);
+        if (checkAllBlock()) {
+            console.log("all lifts are blocked");
+        } else {
+            handleLiftBtn(userEnterdFloor);
+        }
     })
 })
 
-// up_1.addEventListener('click', () => { upLift(floorClasses[0]) })
-// up_2.addEventListener('click', () => { upLift(floorClasses[1]) })
-// up_3.addEventListener('click', () => { upLift(floorClasses[2]) })
-// up_4.addEventListener('click', () => { upLift(floorClasses[3]) })
-
-// down_2.addEventListener('click', () => { downLift(floorClasses[1]) })
-// down_3.addEventListener('click', () => { downLift(floorClasses[2]) })
-// down_4.addEventListener('click', () => { downLift(floorClasses[3]) })
-// down_5.addEventListener('click', () => { downLift(floorClasses[4]) })
-
 
 function switchBtnhandle(e) {
-    if (e.target.checked) {
-        console.log(e.target.id.slice(-1));
-        lift.forEach(x => {
-            if (x.id === `lift-${e.target.id.slice(-1)}`) {
-                removeFloorClesses()
+    // console.log(e.target.classList[0]);
+    const indexOfLift = e.target.classList[0];
+    const checkedOrNot = e.target.checked;
+
+
+    if (checkedOrNot) {
+        lift.forEach((x, index) => {
+            if (x.classList[0] === `${indexOfLift}`) {
+                console.log(index);
+                liftFloorData[index] = Number.NEGATIVE_INFINITY
+
+                moveLift(x, indexOfLift, 1);
                 x.classList.add("blocked");
             }
         })
-    } else if (e.target.checked === false) {
-        lift.forEach(x => {
-            if (x.id === `lift-${e.target.id.slice(-1)}`) {
-                removeFloorClesses();
+    } else if (checkedOrNot === false) {
+        lift.forEach((x, index) => {
+            if (x.classList[0] === `${indexOfLift}`) {
+                liftFloorData[index] = 1
+                moveLift(x, indexOfLift, 1);
                 x.classList.remove("blocked");
             }
         })
     }
+}
+
+function checkAllBlock() {
+    let count = 1;
+    lift.forEach(ele => {
+        if (ele.classList.contains("blocked")) {
+            count++;
+        }
+    });
+    return true ? count > numberOfLift : false;
 }
 
 function findNearestLift(floorNum) {
@@ -92,8 +107,8 @@ function findNearestLift(floorNum) {
         return Math.trunc(Math.random() * numberOfLift);
     }
 
-    let min = 5, minIndex = 0;
-    for (let i = 0; i < liftFloorData.length; i++) {
+    let min = numberOfFloor, minIndex = 0;
+    for (let i = 0; i < numberOfLift; i++) {
         if ((Math.abs(floorNum - liftFloorData[i])) < min) {
             min = Math.abs(floorNum - liftFloorData[i]);
             minIndex = i;
@@ -103,19 +118,16 @@ function findNearestLift(floorNum) {
 }
 
 function handleLiftBtn(floorNum) {
-    // randomNum = Math.trunc(Math.random() * numberOfLift);
 
     nearestLift = findNearestLift(floorNum);
+
     liftFloorData[nearestLift] = floorNum;
     console.log(liftFloorData);
 
-    removeFloorClesses();
-    lift[nearestLift].classList.add(`floor-${floorNum}`);
-    lift[nearestLift].querySelector("span").textContent = liftFloorData[nearestLift]
+    moveLift(lift[nearestLift], nearestLift, floorNum);
 }
 
-function removeFloorClesses() {
-    floorClasses.forEach(floor => {
-        lift[nearestLift].classList.remove(floor);
-    })
+function moveLift(lift, index, floor) {
+    lift.style.bottom = `${(100 * (floor - 1))}px`
+    lift.querySelector("span").textContent = floor;
 }
